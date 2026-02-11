0f2bb72c89037e8347060a96f250dc72f8a2f66a
HX3 Developer <info@keyboardpartner.de>
2026-02-11 15:14:47 +0100

    moved old files

--- make_fatar.bat
+++
@@ -1,8 +0,0 @@
-rem Assemble and convert hx_fatar.psm
-kcpsm6 hx_fatar.psm
-hex2dat_kcpsm6 hx_fatar.hex 2
-copy hx_fatar.dat scanfatr.dat
-copy hx_fatar.fmt hx_fatar.psm
-copy midi_io.fmt midi_io.psm
-del *.fmt
-del *.hex
