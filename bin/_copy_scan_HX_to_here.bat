38ea57bf4dc6317ee700968bc4eb527dbef198e5
HX3 Developer <info@keyboardpartner.de>
2026-03-12 20:12:55 +0100

    new standard state machine

---
+++ bin/_copy_scan_HX_to_here.bat
@@ -0,0 +1,10 @@
+copy E:\Dropbox\HX3.6-3.7-Scan-Drivers\bin\scan_HX_pulse61.dat scanPL61.dat
+copy E:\Dropbox\HX3.6-3.7-Scan-Drivers\bin\scan_HX_fatar1_61.dat scanFT61.dat
+copy E:\Dropbox\HX3.6-3.7-Scan-Drivers\bin\scan_HX_fatar1_73.dat scanFT73.dat
+copy E:\Dropbox\HX3.6-3.7-Scan-Drivers\bin\scan_HX_fatar2.dat scanFATR.dat
+copy E:\Dropbox\HX3.6-3.7-Scan-Drivers\bin\scan_HX_midi.dat scanMIDI.dat
+copy E:\Dropbox\HX3.6-3.7-Scan-Drivers\bin\scan_HX_midi.dat scanBKUP.dat
+copy E:\Dropbox\HX3.6-3.7-Scan-Drivers\bin\scan_HX_midi.dat scan.dat
+copy E:\Dropbox\HX3.6-3.7-Scan-Drivers\bin\scan_HX_scan61.dat scanSR61.dat
+copy E:\Dropbox\HX3.6-3.7-Scan-Drivers\bin\scan_HX_xb25.dat scanXB25.dat
+pause
